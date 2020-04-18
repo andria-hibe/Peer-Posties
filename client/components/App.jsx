970c78384fe018ab62317e23772636e62cc96f46
Andria-Hibe <andriacohibe@gmail.com>
2020-04-18 23:42:36 +1200

    Setup hash router for React

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+
 import MainSection from './MainSection'
+import Dashboard from './Dashboard'
 
 const App = () => {
   return (
-    <>
-      <MainSection/>
-    </>
+    <Router>
+      <Route exact path='/' component={MainSection}/>
+      <Route path='/dashboard' component={Dashboard}/>
+    </Router>
   )
 }
 
--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render} from 'react-dom'
+import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
@@ -13,11 +13,9 @@ const store = createStore(reducers, composeEnhancers(
   applyMiddleware(thunkMiddleware)
 ))
 
-document.addEventListener('DOMContentLoaded', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('app')
-  )
-})
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
